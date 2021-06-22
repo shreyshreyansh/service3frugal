3485e859dda65b5132e09c2b50e9e200a9918280
Shreyansh shrey <53744971+shreyshreyansh@users.noreply.github.com>
2021-06-22 16:17:52 +0530

    Delete router directory

--- router/getalldevices.js
+++
@@ -1,9 +0,0 @@
-const deviceData = require("../database/connect");
-module.exports = (req, res) => {
-  deviceData.find({}, function (err, doc) {
-    if (err) res.send({ status: err });
-    else {
-      res.send({ count: Object.keys(doc).length, results: doc });
-    }
-  });
-};
